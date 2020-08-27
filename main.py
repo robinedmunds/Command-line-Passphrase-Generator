4d1716ebb2dfa3109a518d2e56cc380245199b2e
Robin Edmunds <r.edmunds@live.co.uk>
2020-08-27 21:41:48 +0100

    Manually test Word class

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from diceware.parse_wordlist import from_file as parse_wordlist_from_file
 from diceware.classes.word import Word
 
 
 def main():
-    print("oh hai")
+    wordlist = parse_wordlist_from_file("wordlists\\eff_short_wordlist_1.txt")
+    word_obj = Word(wordlist=wordlist)
+    print(word_obj.word, word_obj.wordlist_key)
 
 
 if __name__ == "__main__":
