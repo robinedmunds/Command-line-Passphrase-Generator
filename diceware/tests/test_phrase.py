0760faa13e5447c610ca0534d85444a20bc8fe67
Robin Edmunds <r.edmunds@live.co.uk>
2020-08-28 14:24:34 +0100

    Add parse_wordlist func to tests

--- diceware/tests/test_phrase.py
+++ diceware/tests/test_phrase.py
@@ -1,42 +1,39 @@
 from ..classes.phrase import Phrase
 from ..parse_wordlist import from_file as parse_wordlist_from_file
 
+# __init__ args: wordlist, word_count, separator
 
-def test_phrase_word_count():
-    wordlist = parse_wordlist_from_file(
+
+def parse_wordlist():
+    return parse_wordlist_from_file(
         "wordlists\\eff_short_wordlist_1.txt")
 
+
+def test_phrase_word_count():
+    wordlist = parse_wordlist()
     phrase_obj = Phrase(wordlist, 3, ".")
     assert phrase_obj.word_count == 3
 
 
 def test_phrase_words_match_count():
-    wordlist = parse_wordlist_from_file(
-        "wordlists\\eff_short_wordlist_1.txt")
-
+    wordlist = parse_wordlist()
     phrase_obj = Phrase(wordlist, 3, ".")
     assert len(phrase_obj.word_objects) == 3
 
 
 def test_phrase_words_separator():
-    wordlist = parse_wordlist_from_file(
-        "wordlists\\eff_short_wordlist_1.txt")
-
+    wordlist = parse_wordlist()
     phrase_obj = Phrase(wordlist, 3, ".")
     assert phrase_obj.separator == "."
 
 
 def test_phrase_as_dict_method_returns_dict():
-    wordlist = parse_wordlist_from_file(
-        "wordlists\\eff_short_wordlist_1.txt")
-
+    wordlist = parse_wordlist()
     phrase_obj = Phrase(wordlist, 3, ".")
     assert isinstance(phrase_obj.as_dict(), dict)
 
 
 def test_phrase_as_string_method_returns_string():
-    wordlist = parse_wordlist_from_file(
-        "wordlists\\eff_short_wordlist_1.txt")
-
+    wordlist = parse_wordlist()
     phrase_obj = Phrase(wordlist, 3, ".")
     assert isinstance(phrase_obj.as_string(), str)
--- diceware/tests/test_word.py
+++ diceware/tests/test_word.py
@@ -1,16 +1,23 @@
 from ..classes.word import Word
 from ..parse_wordlist import from_file as parse_wordlist_from_file
 
+# __init__ args: wordlist
+
+
+def parse_wordlist():
+    return parse_wordlist_from_file(
+        "wordlists\\eff_short_wordlist_1.txt")
+
 
 def test_word_word_is_type_string():
-    wordlist = parse_wordlist_from_file("wordlists\\eff_short_wordlist_1.txt")
+    wordlist = parse_wordlist()
 
     word_obj = Word(wordlist)
     assert isinstance(word_obj.word, str)
 
 
 def test_word_wordlist_key_is_type_int():
-    wordlist = parse_wordlist_from_file("wordlists\\eff_short_wordlist_1.txt")
+    wordlist = parse_wordlist()
 
     word_obj = Word(wordlist)
     assert isinstance(word_obj.wordlist_key, int)
